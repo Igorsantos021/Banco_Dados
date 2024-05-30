f496675ecd283a796824337c3a5ded6ea8d9323d
Igor Santos <santosigor868@gmail.com>
2024-05-29 22:13:25 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_Dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=bd_pokemon;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM tb_pokemon", conn);
+
+            // DataTable = armazenar os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            // MySqlDataAdapter = preencher o DataTable com os resltados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            //Vincula o DataTable ao DataGridView para exibir os dados na grade
+            dataGridView1.DataSource = dt;
+        }
+
+        private void txtpesquisa_TextChanged(object sender, EventArgs e)
+        {
+            string pesquisa = txtpesquisa.Text;
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=bd_pokemon;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM tb_pokemon where nome like '%" + pesquisa + "%'", conn);
+
+            //DataTable = armazenar os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            // MySqlDataAdapter = preencher o DataTable com os resltados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            //Vincula o DataTable ao DataGridView para exibir os dados na grade
+            dataGridView1.DataSource = dt;
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form fcadastro = new Form2();
+            fcadastro.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_Dados
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=bd_pokemon;password=;");
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO tb_pokemon (nome, tipo) VALUES (@Nome, @Tipo)", conn);
+
+            // Inserção no Banco de Dados
+
+            cmd.Parameters.AddWithValue("@Nome", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Tipo", textBox2.Text);
+
+
+            //Abrindo Conexão
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+
+
+            //Validando a Inserção de Dados
+
+            if (rowsAffected > 0)
+            {
+                MessageBox.Show("Dados inseridos com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Falha ao inserir dados.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+            Form fcadastro = new Form1();
+            fcadastro.Show();
+
+
+
+
+
+
+        }
+    }
+}
